1020cece185f49691be27f60bb6c2eda29a08be5
Solal <arroues10@gmail.com>
2019-12-02 07:20:55 +0200

    My first commit

---
+++ src/main/java/com/solal/entity/Mechanic.java
@@ -0,0 +1,45 @@
+package com.solal.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "mechanic")
+public class Mechanic {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private long id;
+	@Column(name = "name")
+	private String name;
+
+	public Mechanic() {
+
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Mechanic [id=" + id + ", name=" + name + "]";
+	}
+}
