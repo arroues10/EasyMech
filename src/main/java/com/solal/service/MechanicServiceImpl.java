bec77340243b6610ed830c5509db6dec5bf08240
Solal <arroues10@gmail.com>
2019-12-16 15:15:51 +0200

    16/12/2019 15:15

--- src/main/java/com/solal/service/MechanicServiceImpl.java
+++ src/main/java/com/solal/service/MechanicServiceImpl.java
@@ -8,31 +8,39 @@ import org.springframework.stereotype.Service;
 import com.solal.entity.Mechanic;
 import com.solal.entity.Part;
 import com.solal.repository.MechanicRepository;
+import com.solal.repository.PartRepository;
+import com.solal.repository.WorkCardRepository;
 
 @Service
 public class MechanicServiceImpl implements MechanicService {
 
 	private long mechanicId;
 	private MechanicRepository mechanicRepository;
+	private WorkCardRepository workCardRepository;
+	private PartRepository partRepository;
 
 	public void setMechanicId(long mechanicId) {
 		this.mechanicId = mechanicId;
 	}
-	
+
 	@Autowired
-	public MechanicServiceImpl(MechanicRepository mechanicRepository) {
+	public MechanicServiceImpl(MechanicRepository mechanicRepository, WorkCardRepository workCardRepository,
+			PartRepository partRepository) {
 		this.mechanicRepository = mechanicRepository;
+		this.workCardRepository = workCardRepository;
+		this.partRepository = partRepository;
 	}
-	
+
 	public Mechanic getMechanic() {
 		return mechanicRepository.findById(mechanicId).orElse(null);
 	}
 
 	public List<Part> addParts(List<Part> parts) {
-		Mechanic mechanic = getMechanic();
-
-		mechanic.addPartsToWorkCard(parts);
-		mechanicRepository.save(mechanic);
+		for (Part part : parts) {
+			part.setWorkCard(workCardRepository.findById(getMechanic().getWorkCard().getId()).orElse(null));
+			part.setId(0);
+			partRepository.save(part);
+		}
 		return parts;
 	}
 
